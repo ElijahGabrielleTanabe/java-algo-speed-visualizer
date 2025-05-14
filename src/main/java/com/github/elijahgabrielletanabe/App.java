c928dcc8b13ca1b0b54c8d51878e2634cba21e1e
ElijahGabrielleTanabe <elijahgabriellet@gmail.com>
2025-05-13 22:59:08 -0500

    refactor: cleaning up unused files and code

--- src/main/java/com/github/elijahgabrielletanabe/App.java
+++ src/main/java/com/github/elijahgabrielletanabe/App.java
@@ -3,26 +3,9 @@ package com.github.elijahgabrielletanabe;
 import java.io.IOException;
 import java.net.URL;
 
-import com.github.elijahgabrielletanabe.Algorithms.BubbleSort;
-import com.github.elijahgabrielletanabe.Algorithms.MergeSort;
-import com.github.elijahgabrielletanabe.Algorithms.QuickSort;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -33,83 +16,11 @@ public class App extends Application {
 
     private static Scene scene;
 
-    private Parent createContent()
-    {
-        BorderPane root = new BorderPane();
-  
-        VBox leftPanel = createLeftPanel();
-        LineChart<Integer, Integer> lc = createAlgoGraph();
-        //VBox stats = createStatsBox();
-
-        root.setLeft(leftPanel);
-        root.setCenter(lc);
-        //root.setRight(stats);
-        //BorderPane.setAlignment(center, Pos.CENTER);
-
-        return root;
-    }
-
-    private VBox createLeftPanel()
-    {
-        VBox leftPanel = new VBox();
-        leftPanel.getStyleClass().add("vbox");
-        //leftPanel.getStylesheets().add(getFileByString("LeftPanel.css"));
-
-        AlgorithmList<AlgorithmBase> al = new AlgorithmList<>();
-        al.addAllItems(
-            new BubbleSort(),
-            new MergeSort(),
-            new QuickSort()
-        );
-
-        ListView<AlgorithmBase> lv = new ListView<>();
-        lv.setItems(al);
-
-        Button run = new Button("Run");
-
-        VBox buttonContainer = new VBox(run);
-        buttonContainer.setAlignment(Pos.CENTER);
-        buttonContainer.setBackground(new Background(new BackgroundFill(
-            Color.RED, CornerRadii.EMPTY, Insets.EMPTY
-        )));
-
-        leftPanel.getChildren().addAll(lv, buttonContainer);
-
-        return leftPanel;
-    }
-
-    private LineChart createAlgoGraph()
-    {
-        final NumberAxis xAxis = new NumberAxis();
-        final NumberAxis yAxis = new NumberAxis();
-
-        xAxis.setLabel("Sort Size");
-        yAxis.setLabel("Time in Milliseconds");
-
-        LineChart<Integer, Integer> lc = new LineChart(xAxis, yAxis);
-        //lc.getStylesheets().add(getFileByString("LineChart.css"));
-
-        return lc;
-    }
-
-    /* 
-    private VBox createStatsBox()
-    {
-        VBox stats = new VBox();
-        stats.getStyleClass().add("vbox");
-        stats.getStylesheets().add(getFileByString("Stats.css"));
-
-        return stats;
-    }
-    */
-
     @Override
     public void start(Stage stage) throws IOException 
     {
-        //scene = new Scene(createContent(), 1100, 650);
-        //scene.getStylesheets().add(getFileByString("App.css", "css").toExternalForm());
-
         scene = new Scene(FXMLLoader.load(getFileByString("sample.fxml", "fxml")));
+        scene.getStylesheets().add(getFileByString("App.css", "css").toExternalForm());
 
         stage.setResizable(false);
         stage.setScene(scene);
--- src/main/java/com/github/elijahgabrielletanabe/Controller.java
+++ src/main/java/com/github/elijahgabrielletanabe/Controller.java
@@ -1,34 +1,73 @@
 package com.github.elijahgabrielletanabe;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 
 public class Controller implements Initializable
 {
-    @FXML
-    private LineChart<?, ?> lineChart;
+    @FXML private LineChart<String, Double> lineChart;
 
-    @FXML
-    private NumberAxis x;
-    
-    @FXML
-    private NumberAxis y;
+    @FXML private VBox sortList;
+    @FXML private ScrollPane sortListContainer;
+
+    @FXML private CategoryAxis x;
+    @FXML private NumberAxis y;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) 
     {
-        System.out.println("Hello World!");
+        System.out.println("Running!");
+
+        //# Populate SortList
+        VBox.setVgrow(sortListContainer, Priority.ALWAYS);
+        
+        
+        //# Testing data
+        XYChart.Series<String, Double> series = new XYChart.Series<>();
+        series.setName("Some Sort");
+        
+        series.getData().add(new XYChart.Data<>("100", 20D));
+        series.getData().add(new XYChart.Data<>("500", 200D));
+        series.getData().add(new XYChart.Data<>("1000", 330D));
+        series.getData().add(new XYChart.Data<>("5000", 420D));
+        series.getData().add(new XYChart.Data<>("10000", 550D));
+
+        //# Have user specify sort sizes, into arraylist for usage in tests and GUI
+        ObservableList<String> sortSizes =  FXCollections.observableList(new ArrayList<>());
+        // Default sort size values
+        sortSizes.addAll(new String[]{"100", "500", "1000", "5000", "10000"});
+
+        x.setCategories(sortSizes);
+
+        lineChart.getData().addAll(series);
     }
 
     @FXML
-    void printHelloWorld(MouseEvent event) 
+    public void queueSort(MouseEvent event)
     {
-        System.out.println("Hello World!");
+        if (event.getSource() == "aButtonfrfr")
+        {
+            System.out.println("Ayeee!");
+        }
+    }
+
+    @FXML
+    public void runTest(MouseEvent event) 
+    {
+
     }
 }
